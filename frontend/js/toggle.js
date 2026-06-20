f9449b1670307637a0f65db2281cf9b4c044d254
akshay-kumar-being-infinity <akshay@being-infinity.com>
2026-06-20 11:40:40 +0530

    Added gitpulse and random user

---
+++ frontend/js/toggle.js
@@ -0,0 +1,32 @@
+var users = [
+    {
+        "name" : "John doe",
+        "gender" : "male",
+        "image" : "/images/john.png"
+    },
+    {
+        "name" : "Jane doe",
+        "gender" : "female",
+        "image" : "/images/jane.png"
+    }
+]
+
+var curId = 0;
+
+function toggle(){
+    // toggle curId from 0 -> 1 & 1 -> 0
+    curId = (curId + 1) % 2;
+
+    /* toggle the rendered user details */
+
+    // image
+    var user = users[ curId ];
+    document.getElementById("user-img").src=user.image;
+    
+    // name
+    document.getElementById("user-name").innerText=user.name
+
+    // gender
+    document.getElementById("user-gender").innerText=user.gender;
+
+}
